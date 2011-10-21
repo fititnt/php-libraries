8912aaa9b911c50d6d71861c3654cced5a923788
fititnt <emerson@webdesign.eng.br>
2011-10-21 16:40:33 -0200

    Few changes to all phpclasses directory structure and etc + Added missing doc folders (ok, have just one @todo, I know) + Added tests folders ^ Changed few folders names (like tests to examples)

--- Gambiarra/etc..js
+++
@@ -1,8 +0,0 @@
-function getDocHeight() { //http://james.padolsey.com/javascript/get-document-height-cross-browser/
-            return Math.max(
-                document.getElementById('main-coluna-a-1').offsetHeight , document.getElementById('main-coluna-c-1').offsetHeight 
-            );
-        }
-/*document.getElementById('main-coluna-b-1').style.height = document.getElementById('main-coluna-a-1').offsetHeight + 'px';*/
-
-document.getElementById('main-coluna-b-1').style.height = getDocHeight() + 'px';
