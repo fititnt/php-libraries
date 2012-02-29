dc78439c17c33abf1b9b6ebd9a1834b20489cb73
fititnt <emerson@webdesign.eng.br>
2012-02-29 03:12:06 -0200

    Improved examples, and added new methods and variables: + Added variable $content + Added variable $header + Added method setContent() + Added method setHeader() + Added method setHeaderCache() + Added method setHeaderMimeType() + Added method getHeader()

--- PHPtoText/examples/example-javascript.php
+++ PHPtoText/examples/example-javascript.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * @package         DBParser
+ * @package         PHPtoText
  * @author          Emerson Rocha Luiz - emerson at webdesign.eng.br - http://fititnt.org
- * @copyright       Copyright (C) 2011 Webdesign Assessoria em Tecniligia da Informacao. All rights reserved.
+ * @copyright       Copyright (C) 2012 Webdesign Assessoria em Tecnilogia da Informacao. All rights reserved.
  * @license         GNU General Public License version 3. See license-gpl3.txt
  * @license         Massachusetts Institute of Technology. See license-mit.txt
  * @version         0.1alpha
@@ -13,5 +13,25 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); //better debug
 
 include_once '../library/PHPtoJavascript.php';
 
-$phptext = new PHPtoJavascript;
-var_dump($phptext);
+$phpjs = new PHPtoJavascript;
+
+$phpjs->setContent('alert("hi")');
+$phpjs->setContent('alert("hello")');
+$phpjs->setHeaderMimeType('js');
+$phpjs->setHeaderCache(FALSE);
+$phpjs->getHeader(true);
+
+//Cache-Control	no-cache, must-revalidate
+//Connection	Keep-Alive
+//Content-Length	27
+//Content-Type	application/javascript
+//Date	Wed, 29 Feb 2012 04:56:37 GMT
+//Expires	Sun, 25 Jan 1986 05:00:00 GMT
+//Keep-Alive	timeout=5, max=100
+//Server	Apache/2.2.21 (Win32) PHP/5.3.8
+//X-Powered-By	PHP/5.3.8
+
+echo $phpjs->getContent();
+//alert("hi")
+//alert("hello")
+//var_dump($phpjs);
--- PHPtoText/examples/example-string.php
+++ PHPtoText/examples/example-string.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * @package         DBParser
+ * @package         PHPtoText
  * @author          Emerson Rocha Luiz - emerson at webdesign.eng.br - http://fititnt.org
- * @copyright       Copyright (C) 2011 Webdesign Assessoria em Tecniligia da Informacao. All rights reserved.
+ * @copyright       Copyright (C) 2012 Webdesign Assessoria em Tecnilogia da Informacao. All rights reserved.
  * @license         GNU General Public License version 3. See license-gpl3.txt
  * @license         Massachusetts Institute of Technology. See license-mit.txt
  * @version         0.1alpha
@@ -14,4 +14,16 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE); //better debug
 include_once '../library/PHPtoString.php';
 
 $phpstring = new PHPtoString;
-var_dump($phpstring);
+$phpstring->setContent('Line 1');
+$phpstring->setContent('Line 2');
+$phpstring->setContent('Line 3');
+$phpstring->setContent('Line 4');
+
+echo '<pre>';
+echo $phpstring->getContent();
+//Line 1
+//Line 2
+//Line 3
+//Line 4
+
+//var_dump($phpstring);
